5fdd7691912e22322c16858a635c4dfc996facb8
James Rogers <jarogers095@gmail.com>
2019-12-03 21:43:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(source_array)
   return_array = []
   
   source_array.each do |element|
-    return_array.push(yield)
+    return_array.push(yield(element))
   end
   
   return return_array
